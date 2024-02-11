353128db822837a3a3b8fcbb3b69353f5679bee1
pratham534 <prathamsrivastava534@gmail.com>
2024-02-12 01:07:02 +0530

    created models for messages and room for chat

--- babble/asgi.py
+++ babble/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'babble.settings')
 
-application = get_asgi_application()
+from chat import routing
+
+django_asgi_application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': django_asgi_application,
+    'websocket': AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)))
+})
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+
+class Message(models.Model):
+    body = models.TextField()
+    sent_by = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.sent_by}'
+    
+
+class Room(models.Model):
+    WAITING = 'waiting'
+    ACTIVE = 'active'
+    CLOSED = 'closed'
+
+    STATUS_CHOICES = [
+        (WAITING, 'waiting'),
+        (ACTIVE, 'active'),
+        (CLOSED, 'closed'),
+    ]
+
+    roomid = models.CharField(max_length=150, unique=True)
+    client = models.CharField(max_length=150)
+    agent = models.ForeignKey(User, max_length=150, related_name='rooms', blank=True, null=True, on_delete=models.SET_NULL)
+    messages = models.ManyToManyField(Message, blank=True)
+    url = models.CharField(max_length=250, blank=True, null=True)
+    status = models.CharField(max_length=150, choices=STATUS_CHOICES, default=WAITING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'{self.client} - {self.roomid}'
